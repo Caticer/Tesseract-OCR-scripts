f23a30b94a4049c7c1c84931fb90ad0e1d27d026
İbrahim YÖNDEMLİ <doopthe@gmail.com>
2021-04-16 17:44:30 +0300

    nothing

--- merge.bat
+++ merge.bat
@@ -1,5 +1,9 @@
 @echo off
 title Merge PDF files
+@shift /0
+mode con cols=70 lines=20
+
+
 if exist ".\out\out.pdf" del /s /f /q ".\out\out.pdf"
 if exist ".\tmp\*.pdf" del /s /f /q ".\tmp\*.pdf"
 if exist ".\input\*" del /s /f /q ".\input\*"
